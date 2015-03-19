d9e941038519c09c1602c271c907310b2091109c
alfred sang <i5ting@126.com>
2015-03-19 13:48:14 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # node-cli-tmpl
+
+
+cli init
--- tmpl.js
+++ tmpl.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-require('shelljs/global');
 var child_process = require('child_process');
 var subl = '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl';
 
@@ -10,19 +9,10 @@ argv.shift();
 var file_path = __dirname;
 var current_path = process.cwd();
 
-var cp_files = ['index.js', 'package.json', 'LICENSE', 'README.md'];
-
 if ( argv.length > 1 ) {
   var first_arg = argv[0];
   if ( first_arg == '-h' || first_arg == '--help' ) {
  
-  }
-  
-  if ( first_arg == 'init' ) {
-    file_path.forEach(function(path){
-      cp('-f', file_path + '/' + path, current_path);  
-    });
-    
   }
 }
 
